5a15f0222e4e22a47c4e9d5f8d338cfe68d35df8
Abhi8099 <abhishek.sharma1@digranknow.com>
2025-02-10 15:42:51 +0530

    fi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hello = () => {
   return (
     <div className='flex text-3xl font-bold h-screen w-full items-center justify-center'>
-      Hello
+      Hello world
     </div>
   )
 }
